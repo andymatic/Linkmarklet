c4dc1c5942c64a3dadce698fd5864c785b448cf5
Andy Wibbels <andymatic@gmail.com>
2013-03-24 00:07:23 -0700

    A few CSS changes, taking tags back out since they aren't working yet

--- index.php
+++ index.php
@@ -157,7 +157,7 @@ function linkmarklet_post()
             position:absolute;
             left:8px;
             top:12px;
-            color:#bbb;
+            color:#777;
         }
         div.field input {
             display:block;
@@ -251,10 +251,6 @@ function linkmarklet_post()
             <label for="content">Content</label>
             <textarea name="content" id="content"><?php echo $selection; ?></textarea>
         </div>
-            <div class="field textfield" id="row-tags">
-                <label for="url">Tags</label>
-                <input type="text" name="tags" id="tags" value="" />
-            </div>
         <div class="field textfield" id="row-title">
             <label for="title">Title</label>
             <input type="text" name="title" id="title" value="<?php echo $title; ?>" />
@@ -272,10 +268,6 @@ function linkmarklet_post()
         var title = document.getElementById('row-title').offsetHeight;
         var url = document.getElementById('row-url').offsetHeight;
         var slug = document.getElementById('row-slug').offsetHeight;
-        <?php if( !empty( $settings['support_tags'] ) ) : ?>
-        var tags            = document.getElementById('row-tags').offsetHeight;
-        height = height - tags;
-        <?php endif; ?>
         var height = window_height - actions - title - url - slug - 25;
         document.getElementById('content').style.height = height + 'px';
     }
